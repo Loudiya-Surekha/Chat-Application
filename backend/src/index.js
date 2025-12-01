6efe621563e4d4ccb7d603eaed86cf500661f214
ADMIN <lsurekha2002@gmail.com>
2025-12-01 19:15:42 +0530

    ssss

--- backend/src/index.js
+++ backend/src/index.js
@@ -38,7 +38,7 @@ app.use("/messages", messageRoutes)
 if (process.env.NODE_ENV=== "production") {
    app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-   app.get("*", (req, res) => {
+   app.get("/", (req, res) => {
    if (!req.path.startsWith("/")) {
       res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
     }
@@ -46,16 +46,6 @@ if (process.env.NODE_ENV=== "production") {
    })
 }
 
-
-// if (process.env.NODE_ENV === "production") {
-//   const frontendPath = path.join(__dirname, "../frontend/dist");
-
-//   app.use(express.static(frontendPath));
-
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(frontendPath, "index.html"));
-//   });
-// }
 //   app.get("/",(req,res)=>{
 //    res.json("hello surekha")
 // })
